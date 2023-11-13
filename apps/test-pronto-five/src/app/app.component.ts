459a134c071cfec32167d6c48eecd5e40278fe0f
Charlie Greenman <charlie@razroo.com>
2023-11-13 03:10:24 -0500

    Add code for angular-16.2.0 create-workspace.
    [Url: razroo.com/community/angular-16.2.0/create-workspace-recipe/create-workspace]

---
+++ apps/test-pronto-five/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'test-pronto-five-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'test-pronto-five';
+}
